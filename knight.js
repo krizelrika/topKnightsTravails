1621abe8da86ac144677ba7b38a07383610b2a7f
Krizel Rika <krizelcodes@gmail.com>
2025-12-01 19:31:34 +0800

    possible moveset, check coords

---
+++ knight.js
@@ -0,0 +1,10 @@
+// Knight's possible moves on a chessboard
+const knightMoveset = [
+  [2, 1], [1, 2], [-1, 2], [-2, 1],
+  [-2, -1], [-1, -2], [1, -2], [2, -1],
+];
+
+// Check if a coordinate is on the 8x8 board
+function isValid([x, y]) {
+  return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
